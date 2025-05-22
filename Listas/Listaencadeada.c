2ca6454793a1375dd6645306fdb9f6ba183db749
Gustavo Pavanelli <69548993+PavanelliGustavo@users.noreply.github.com>
2025-05-22 14:37:58 -0300

    Listaencadeada.c

--- Listas/Listaencadeada.c
+++ Listas/Listaencadeada.c
@@ -18,9 +18,17 @@ void add(metro* l, int numeracao, char passageiro) {
     vagao* v0 = malloc(sizeof(vagao));
     v0->numeracao = numeracao;
     v0->passageiro = passageiro;
-    v0->prox = l->inicio;
-    v0->anterior = l->final;
-    l->inicio = v0;
+    if(l->tamanho != 0) {
+        v0->prox = l->inicio;
+        v0->anterior = l->final;
+        l->inicio = v0;
+    }
+    else {
+        v0->prox = v0;
+        v0->anterior = v0;
+        l->inicio = v0;
+        l->final = v0;
+    }
     l->tamanho++;
 }
 
@@ -43,7 +51,7 @@ void addfinal(metro* l, int numeracao, char passageiro) {
     l->tamanho++;
 }
 
-void remove(metro* l, int numeracao) {
+void remover(metro* l, int numeracao) {
     if(l->tamanho != 0) {
         vagao* atual = l->inicio;
         int contador = 0;
@@ -60,22 +68,24 @@ void remove(metro* l, int numeracao) {
 
 void printar(metro* l) {
     vagao* atual = l->inicio;
-    while(atual->prox != l->final) {
+    int contador = 0;
+    while(contador < l->tamanho) {
         printf("numeracao vagao = %d e passageiro vagao = %c\n", atual->numeracao, atual->passageiro);
         atual = atual->prox;
+        contador++;
     }
 }
 
 int main() {
-    metro m0;
-    m0.tamanho = 0;
-    m0.inicio = NULL;
-    m0.final = m0.inicio;
-    add(&m0, 0, 'a');
-    add(&m0, 1, 'b');
-    printar(&m0);
-    add(&m0, 2, 'c');
-    addfinal(&m0, 3, 'd');
-    add(&m0, 4, 'e');
-    remove(&m0, 2);
+    metro* m0 = malloc(sizeof(metro));
+    m0->tamanho = 0;
+    m0->inicio = NULL;
+    m0->final = NULL;
+    add(m0, 0, 'a');
+    add(m0, 1, 'b');
+    printar(m0);
+    add(m0, 2, 'c');
+    addfinal(m0, 3, 'd');
+    add(m0, 4, 'e');
+    remover(m0, 2);
 }
