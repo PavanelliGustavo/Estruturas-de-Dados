2a30d2ea39dc2857c3b2a006aa83ac94bcc927d7
Gustavo Pavanelli <69548993+PavanelliGustavo@users.noreply.github.com>
2025-06-01 22:41:56 -0300

    Update Pilhas.c

--- Pilhas/Pilhas.c
+++ Pilhas/Pilhas.c
@@ -13,7 +13,7 @@ typedef struct lista{
     int tamanho;
 } lista;
 
-void criar(char nome) {
+void stack(char nome) {
     typedef struct NO2{
         int numeracao;
         struct NO2* prox;
@@ -32,7 +32,7 @@ void criar(char nome) {
     n0->final = NULL;
 }
 
-void add(lista* l, int numeracao) {
+void push(lista* l, int numeracao) {
     NO* v0 = malloc(sizeof(NO));
     v0->numeracao = numeracao;
     if(l->tamanho != 0) {
@@ -47,52 +47,29 @@ void add(lista* l, int numeracao) {
     l->tamanho++;
 }
 
-void addfinal(lista* l, int numeracao) {
-    NO* v0 = malloc(sizeof(NO));
-    v0->numeracao = numeracao;
-    if(l->tamanho != 0) {
-        l->final->prox = v0;
-        v0->anterior = l->final;
-        l->final = v0;
-    }
-    else {
-        l->inicio = v0;
-        l->final = l->inicio;
-    }
-    l->tamanho++;
-}
-
-void remover(lista* l, int numeracao) {
+void pop(lista* l) {
     if(l->tamanho == 1) {
-            l->inicio = NULL;
-            l->final = NULL;
-            l->tamanho--;
-            free(l->inicio);
+        l->inicio = NULL;
+        l->final = NULL;
+        free(l->inicio);
     }
-    else if(l->tamanho > 1){
-        NO* atual = l->inicio;
-        while(atual->numeracao != numeracao) {
-            atual = atual->prox;
-        }
-        atual->anterior->prox = atual->prox;
-        atual->prox->anterior = atual->anterior;
-        l->tamanho--;
-        free(atual);
+    else if(l->tamanho > 1) {
+        l->inicio = l->inicio->prox;
+        free(l->inicio->anterior);
     }
+    l->tamanho--;
 }
 
-void printar(lista* l) {
-    NO* atual = l->inicio;
-    int contador = 0;
-    while(contador < l->tamanho) {
-        printf("numeracao NO = %d\n", atual->numeracao);
-        atual = atual->prox;
-        contador++;
-    }
+void peek(lista* l) {
+    printf("%d\n", l->inicio);
 }
 
 void tamanho(lista* l) {
     printf("%d\n", l->tamanho);
+}
+
+void isEmpty(lista* l) {
+    return l->tamanho==0;
 }
 
 int main() {
